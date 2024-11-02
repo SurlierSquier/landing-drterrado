257512d2e760bd7447b2071f645f4a1d57dd393f
Joaquin Terrado Mora <joacoterrado@gmail.com>
2024-11-02 11:52:21 +0100

    Mejorar el diseño del encabezado y la sección de héroe, añadiendo efectos de hover y ajustando estilos en Tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,13 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }) {
+		  addUtilities({
+			'.hover-effect': {
+			 '@apply transition hover:opacity-85 hover:scale-150': {}
+			}
+		  })
+		}
+	  ],
 }
